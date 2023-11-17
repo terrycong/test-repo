5b538f8495cee46c28aa2eed34d1740b290fea94
terrycong <terrycong@gmail.com>
2023-11-18 00:59:00 +0800

    3 commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ import org.apache.commons.lang3.StringUtils;
 
 public class Main {
 public static void main(String[] args) {
-	System.out.println(StringUtils.repeat('2', 100));
+	System.out.println(StringUtils.repeat('3', 100));
 }
 }
